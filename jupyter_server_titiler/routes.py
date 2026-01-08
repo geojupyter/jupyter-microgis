a8f7865a0b8298da731486afa240da865d673c50
Matt Fisher <mfisher87@gmail.com>
2026-01-08 15:51:09 -0700

    Add handling (and test) when no path provided to route

--- jupyter_server_titiler/routes.py
+++ jupyter_server_titiler/routes.py
@@ -1,17 +1,27 @@
 import json
 
-from jupyter_server.base.handlers import JupyterHandler 
+from jupyter_server.base.handlers import APIHandler
 from jupyter_server.utils import url_path_join
 import tornado
 
-from jupyter_server_titiler.constants import ENDPOINT_BASE
+from jupyter_server_titiler.constants import (
+    ENDPOINT_BASE,
+    SERVER_EXTENSION_NAME,
+)
 
 
-class TiTilerRouteHandler(JupyterHandler):
+class TiTilerRouteHandler(APIHandler):
     """How does this handler work?"""
 
     @tornado.web.authenticated
-    async def get(self, path):
+    async def get(self, path: str):
+        if not path:
+            self.finish(
+                f"This is the root endpoint of the '{SERVER_EXTENSION_NAME}'"
+                " server extension"
+            )
+            return
+
         params = {key: val[0].decode() for key, val in self.request.arguments.items()}
         server_url = params.pop("server_url")
         print(path)
--- jupyter_server_titiler/tests/test_routes.py
+++ jupyter_server_titiler/tests/test_routes.py
@@ -1,11 +1,11 @@
 import json
 
+from jupyter_server_titiler.constants import ENDPOINT_BASE
 
-async def test_get_example(jp_fetch):
-    response = await jp_fetch("jupyter-server-titiler", "get-example")
+
+async def test_extension_root_route(jp_fetch):
+    # Not sure why a 2nd argument is needed! ¯\_(ツ)_/¯
+    response = await jp_fetch(ENDPOINT_BASE, "/")
 
     assert response.code == 200
-    payload = json.loads(response.body)
-    assert payload == {
-        "data": "This is /jupyter-server-titiler/get-example endpoint!"
-    }
+    assert response.body == b"This is the root endpoint of the 'jupyter_server_titiler' server extension"
